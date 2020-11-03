162e77a94690910772247fe81449f03f2960039a
Allan Kamimura <allan_kamimura@hotmail.com>
2020-11-03 15:10:25 -0300

    typos and things

--- MMQ.py
+++ MMQ.py
@@ -9,38 +9,39 @@ v2 Created on Tue Jul 28 02:14:04 2020
 
 import matplotlib.pyplot as plt
 import numpy as np
-
 xlista=[]
 xflista=[]#x em float
-ylista=[]#fiz isso pra testar uma coisa
+ylista=[]#fiz isso por questão de estilo
 yflista=[]#y em float
-
 
 def intro():
     print(" ")
-    intro = "O proposito desse programa é fazer uma regressão linear utilizando o Método dos Mínimos Quadrados"
+    intro = "O proposito desse programa é fazer cálculos laboratoriais com erro experimental"
     print("|{0:^100}|".format(intro))
     return Menu()
 
 def Menu(): #menu principal
     while True:
         print("Escolha uma das opções abaixo:")
+        print("   VAR) Cálculo de Variança e Desvio Padrão")
         print("   MMQ) Regreção Linear com Método dos Mínimos Quadrados")
         print("   ANT) Utilizar a vesão antiga do programa")
         print("   LOG) Acessar o change log")
         print("   SAIR) Fecha o programa")
         Escolha = input("Escolha: ")
         Escolha1 = Escolha.lower()
-        if Escolha1 in ["mmq","ant","log","sair"]:
+        if Escolha1 in ["var","mmq","ant","log","sair"]:
             break
         else:
             print(Escolha1 + " não é uma escolha válida. Escolha novamente\n")
             continue #apenas escolhas validas
     if Escolha1 == "mmq":
         return Menu2()
+    if Escolha1 == "var":
+        return Menu3()
     if Escolha1 == "sair":  
         input("Aperte ENTER para sair")
-
+        
 def Menu2():
     while True:
         print("Você escolheu Regreção linear com MMQ")
@@ -63,13 +64,99 @@ def Menu2():
         return sheets()
     if escolha2 == "4":
         return Menu()
+
+def Menu3():
+    while True:
+        print("Você escolheu Variance e Desvio Padrão")
+        print("Agora escolha como inserir os valores:")
+        print("   1) Uma célula de cada vez")
+        print("   2) Uma coluna de cada vez")
+        print("   3) Copiar e colar do google sheets")
+        print("   4) Voltar para o menu anterior")
+        escolha3 = input("Escolha: ")
+        if escolha3 in ["1","2","3","4","5"]:
+            break
+        else:
+            print("\nApenas as opções abaixo: ")
+            continue
+    if escolha3 == "1":
+        return y1()
+    if escolha3 == "2":
+        return xy()
+    if escolha3 == "3":
+        return sheets()
+    if escolha3 == "4":
+        return Menu()
     
-def x1():
-    print("Digite um valor de x, quando acabar, insira o valor 0")
+def y1():
+    print("")
+    print("Digite um valor de y, quando acabar, aperte ENTER")
     print("Exemplo de escrita: 0,66 ou 0.66 ou 66e-2")
     print("Se você inserir um valor errado, escreva denovo, mas com valor certo") 
     print("No final só da pra tirar o que está errado")
-    while True:  
+    while True:
+        a = input("valor de y = ")
+        if a == "":
+            break
+        try: #com isso, o valor y = 0 é permitido
+            af = float(a.replace(',','.')) #isso aqui é proposital, com efeitos esteticos
+            ylista.append(a)
+            yflista.append(af)
+            print("|| {0:^20} ||".format("y"))
+            for numero in ylista:
+                print("|| {0:^20} ||".format(numero))
+            continue
+        except:
+            print("Apenas números e digite dessa forma: 0.66 ou 66e-2")
+            continue
+    while True:
+        print("Ficou assim:\n|| {0:^20} ||".format("y"))
+        for numeroy in ylista:
+            print("|| {0:^20} ||".format(numeroy))
+        certo1 = input("Os valores estão corretos? S ou N? ")
+        if certo1.lower() in ["s","n"]:
+            break
+        else:
+            print("Apenas S ou N")
+            continue
+    if certo1.lower() == "s":
+        return x1()
+    if certo1.lower() == "n":      
+        return corrigiry1()
+
+def corrigiry1():
+    while True: #tirar valor errado
+        print("Ficou assim:\n|| {0:^20} ||".format("y"))
+        for numero in ylista:
+            print("|| {0:^20} ||".format(numero))
+        print("Qual valor você quer tirar?")
+        tirar = input("y = ")
+        try:
+            ylista.remove(tirar)
+            yflista.remove(float(tirar.replace(',','.')))
+            print("Ficou assim:\n|| {0:^20} ||".format("y"))
+            for numeroy in ylista:
+                print("|| {0:^20} ||".format(numeroy))
+            certo1 = input("Ta certo agora? S ou N ")
+            if certo1.lower() == "s":
+                break
+            if certo1.lower() == "n":
+                continue
+            else:
+                print("Apenas S ou N")
+                continue
+        except:
+            print("Apenas números da lista \n")
+            continue
+    return x1()
+        
+def x1():
+    print("")
+    print("Digite um valor de x, quando acabar, aperte ENTER")
+    print("Exemplo de escrita: 0,66 ou 0.66 ou 66e-2")
+    print("Se você inserir um valor errado, escreva denovo, mas com valor certo") 
+    print("No final só da pra tirar o que está errado")
+    while True:
         a = input("valor de x = ")
         try: #com isso, o valor x = 0 é permitido
             if a == "":
@@ -98,7 +185,7 @@ def x1():
         return final()
     if certo1.lower() == "n":      
         return corrigirx1()
-    
+
 def corrigirx1():
     print("|| {0:^20} ||".format("x"))
     while True: #tirar valor errado
@@ -124,68 +211,6 @@ def corrigirx1():
             print("Apenas números da lista \n")
             continue
     return final()
-        
-def y1():
-    print("")
-    print("Digite um valor de y ou digite 0 para prosseguir")
-    print("Exemplo de escrita: 0,66 ou 0.66 ou 66e-2")
-    print("Se você inserir um valor errado, escreva denovo, mas com valor certo") 
-    print("No final só da pra tirar o que está errado")
-    while True:
-        a = input("valor de y = ")
-        if a == "":
-            break
-        try: #com isso, o valor y = 0 é permitido
-            af = float(a.replace(',','.')) #isso aqui é proposital, com efeitos esteticos
-            ylista.append(a)
-            yflista.append(af)
-            print("|| {0:^20} ||".format("y"))
-            for numero in ylista:
-                print("|| {0:^20} ||".format(numero))
-            continue
-        except:
-            print("Apenas números e digite dessa forma: 0.66 ou 66e-2")
-            continue      
-    while True:
-        print("Ficou assim:\n|| {0:^20} ||".format("y"))
-        for numeroy in ylista:
-            print("|| {0:^20} ||".format(numeroy))
-        certo1 = input("Os valores estão corretos? S ou N? ")
-        if certo1.lower() in ["s","n"]:
-            break
-        else:
-            print("Apenas S ou N")
-            continue
-    if certo1.lower() == "s":
-        return x1()
-    if certo1.lower() == "n":      
-        return corrigiry1()
-    
-def corrigiry1():
-    while True: #tirar valor errado
-        print("Ficou assim:\n|| {0:^20} ||".format("y"))
-        for numero in ylista:
-            print("|| {0:^20} ||".format(numero))
-        print("Qual valor você quer tirar?")
-        tirar = input("y = ")
-        try:
-            ylista.remove(tirar)
-            yflista.remove(float(tirar.replace(',','.')))
-            print("Ficou assim:\n|| {0:^20} ||".format("y"))
-            for numeroy in ylista:
-                print("|| {0:^20} ||".format(numeroy))
-            certo1 = input("Ta certo agora? S ou N ")
-            if certo1.lower() == "s":
-                break
-            if certo1.lower() == "n":
-                continue
-            else:
-                print("Apenas S ou N")
-                continue
-        except:
-            print("Apenas números da lista \n")
-            continue
-    return x1()
 
 def xy():
     while True:
@@ -211,7 +236,9 @@ def xy():
             print(ylista,"\n",xlista)
             print("Os valores foram corretamente digitados?")
             continue
-        
+    return final()
+
+
 def sheets():
     while True:
         print("")
@@ -237,8 +264,7 @@ def sheets():
             print("Os valores foram corretamente digitados?")
             continue
     return final()
-
-#vale pros 2 casos
+        
 def final():
     sumx = 0
     vertn = 0
@@ -247,7 +273,6 @@ def final():
     alfan = 0
     xx = 0
     alfan = 0
-    
     for i in range(0,len(xflista)):#calculo das medias
         sumx = sumx + xflista[i]
         sumy = sumy + yflista[i]
@@ -273,8 +298,10 @@ def final():
     for a in range(0,len(xflista)):
         print("||{0:^3}||  {1:^20} ||  {2:^20} ||".format(a+1,yflista[a],xflista[a]))
     print("")
-    print("O coeficiente angular(a ± Δa) é:",alfa,"±",ialfa)
+    print("O coeficiente angular(a ± Δa) é:{} ± {}".format(alfa,ialfa))
     print("O coeficiente linear(b ± Δb) é:",beta,"±",ibeta)
     print("Dispersão média do ajuste:",vert)
-    print("Não se esqueça de truncar os valores quando colocar no relatório")
     print("")
+
+if __name__ == '__main__':
+    intro()
